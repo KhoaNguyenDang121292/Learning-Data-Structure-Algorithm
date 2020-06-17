8cb5d59a9e68ab1ce3c245971472bb94cd7700d5
unknown <kayd200123@gmail.com>
2020-06-17 21:09:27 +0700

    Add Selection Sort Algorithm

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,6 +1,33 @@
-def SelectionSort(arr):
-    minPos = 0
-    i = len(arr) - 1
-    while i >= 1:
-        if arr[i] < minElement:
-            arr[i], minElement = minElement, arr[i]
+# Best case: 
+# Worst case: 
+# Average case: 
+# Time complexity: O(n2)
+# Space complexity: 
+
+from read_sample import ReadSampleData
+
+def SelectionSortAsc(arr):
+    i = 0
+    while i <= len(arr) - 1:
+        minPos = i
+        for j in range(i+1, len(arr), +1):                                                                                                                                                                 
+            if arr[j] < arr[minPos]:
+                arr[j], arr[minPos] = arr[minPos], arr[j]
+        i += 1
+
+def SelectionSortDesc(arr):
+    i = 0
+    while i <= len(arr) - 1:
+        minPos = i
+        for j in range(i+1, len(arr), +1):
+            if arr[j] > arr[minPos]:
+                arr[j], arr[minPos] = arr[minPos], arr[j]
+        i += 1
+
+def SelectionSortOptimizeAsc(arr):
+    
+
+if __name__ == "__main__":
+    arr = ReadSampleData()
+    SelectionSortDesc(arr)
+    print(arr)
--- generate_data.py
+++ generate_data.py
@@ -2,6 +2,6 @@ import random
 
 filePath = "test_data.txt"
 with open(filePath, 'w') as f:
-    for i in range(0, 100, +1):
+    for i in range(0, 20, +1):
         f.write(str(random.randint(0, 1000)))
         f.write(" ")
