105e39ec7a87459e44f3d46e48e87883adc7fc01
unknown <kayd200123@gmail.com>
2020-06-21 16:12:49 +0700

    Linked List is done with KEY CONCEPT and Perform

--- DataStructure/LinkedList.py
+++ DataStructure/LinkedList.py
@@ -1,30 +1,53 @@
+""" KEY CONCEPT:
+    1. Each element has: value and next element
+    2. Each linked list when adding:
+        - Check if HEAD exists.
+        - Iterate until end of linked list and then add new element.
+"""
+
 class Element(object):
-    def __init__(self, value, next):
+    def __init__(self, value):
         self.value = value
-        self.next = next
+        self.next = None
          
 class LinkedList(object):
     def __init__(self, head=None):
         self.head = head
     
-    def add(self, next):
-        current = self.head
-        if current != None:
-            current.next = next
+    def add(self, newElement):
+        # Check if linkedlist already had HEAD
+        if self.head:
+            current = self.head
+            # Iterate to end of this linked list
+            while current.next:
+                current = current.next
+            # Add new element next to last element
+            current.next = newElement
         else:
-            current.next = None
+            self.head = newElement
     
-    def show(self):
-        current = self.head
-        while current.next:
-            print(str(current.value) + "->")
-            current = current.next
+    def traverse(self):
+        # Check if Linked list already had HEAD
+        if self.head:
+            current = self.head
+            # Iterate to end of this linked list
+            while current.next:
+                # Print current node value
+                if current.next:
+                    if current == self.head:
+                        print("HEAD<--" + str(current.value) + "->", end='')
+                    else:
+                        print(str(current.value) + "->", end='')
+                else:
+                    print(str(current.value), end='')
+                current = current.next
+
 
 if __name__ == "__main__":
-    head = Element(0, None)
-    node_1 = Element(1, None)
-    linkedList = LinkedList(head=head)
-    linkedList.add(node_1)
-    linkedList.show()
+    head = Element(1)
+    node_1 = Element(2)
+    llist = LinkedList(head=head)
+    llist.add(node_1)
+    llist.traverse()
     # arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     
